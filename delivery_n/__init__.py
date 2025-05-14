743f96964704b79e3b019f9f3a1e929430ab443c
H2py <ahdosj@gmail.com>
2025-05-14 13:04:16 +0900

    [Feat] Add requirements.txt

--- delivery_n/__init__.py
+++ delivery_n/__init__.py
@@ -6,6 +6,8 @@ from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
+# pip install -r requirements.txt
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -27,26 +29,6 @@ def create_app(test_config=None):
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint="index")
-    
-    @app.route("/login", methods=["POST"])
-    def login():
-        username = request.json.get("username", None)
-        password = request.json.get("password", None)
-        if username != "test" or password != "test":
-            return jsonify({"msg": "Bad username or password"}), 401
-
-        access_token = create_access_token(identity=username)
-        return jsonify(access_token=access_token)
-
-
-
-    @app.route("/protected", methods=["GET"])
-    @jwt_required()
-    def protected():
-        # Access the identity of the current user with get_jwt_identity
-        current_user = get_jwt_identity()
-        return jsonify(logged_in_as=current_user), 200
-
 
     return app
         
